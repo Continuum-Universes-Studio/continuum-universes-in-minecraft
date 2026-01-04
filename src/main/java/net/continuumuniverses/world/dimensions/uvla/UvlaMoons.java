9843cf04b29705e5511a8da6f6aa83f4cb20b28b
Christian Daniel Jensen <cdjensen94@gmail.com>
2026-01-04 12:33:30 -0700

    Speed up Uvla moon orbits

--- src/main/java/net/continuumuniverses/world/dimensions/uvla/UvlaMoons.java
+++ src/main/java/net/continuumuniverses/world/dimensions/uvla/UvlaMoons.java
@@ -12,7 +12,7 @@ public final class UvlaMoons {
             0.0F,
             3.0F,
             0.6F,
-            14.0F,
+            6.0F,
             0.0F // distance (unused right now)
     );
 
@@ -23,7 +23,7 @@ public final class UvlaMoons {
             15.0F,
             4.5F,
             0.3F,
-            28.0F,
+            10.0F,
             0.0F
     );
 
@@ -34,7 +34,7 @@ public final class UvlaMoons {
             35.0F,
             6.5F,
             0.45F,
-            32.0F,
+            12.0F,
             0.0F
     );
 
