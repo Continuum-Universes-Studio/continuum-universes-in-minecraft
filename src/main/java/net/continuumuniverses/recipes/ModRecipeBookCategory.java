35ebe0d470a9709d9dae082b907e85944ca4c58c
Christian Daniel Jensen <cdjensen94@gmail.com>
2026-01-15 01:21:43 -0700

    Add plasma recipe book tab

--- src/main/java/net/continuumuniverses/recipes/ModRecipeBookCategory.java
+++ src/main/java/net/continuumuniverses/recipes/ModRecipeBookCategory.java
@@ -1,13 +1,21 @@
 package net.continuumuniverses.recipes;
+
+import net.continuumuniverses.ContinuumUniverses;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeBookCategory;
 
-public class ModRecipeBookCategory{
+public class ModRecipeBookCategory {
     public static final RecipeBookCategory PLASMA_SMELTING = register("plasma_smelting");
     public static final RecipeBookCategory STELLAR_FORGE = register("stellar_forge");
     public static final RecipeBookCategory LUNAR_FORGE = register("lunar_forge");
+
     private static RecipeBookCategory register(String name) {
-        return Registry.register(BuiltInRegistries.RECIPE_BOOK_CATEGORY, name, new RecipeBookCategory());
+        return Registry.register(
+                BuiltInRegistries.RECIPE_BOOK_CATEGORY,
+                ResourceLocation.fromNamespaceAndPath(ContinuumUniverses.MODID, name),
+                new RecipeBookCategory()
+        );
     }
 }
